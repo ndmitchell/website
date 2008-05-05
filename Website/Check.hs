11057c646e41687ed68878176eb68f5a125e8c6d
Neil Mitchell <ndmitchell@gmail.com>
2008-05-05 15:16:09 +0000

    Add progress reporting to the validity checking

--- Website/Check.hs
+++ Website/Check.hs
@@ -12,16 +12,17 @@ check :: [String] -> IO ()
 check xs = do
     failed <- liftM sum $ mapM checkOne xs
     if failed == 0 then
-        putStrLn $ "Success, no invalid pages"
+        putStrLn $ "\nSuccess, no invalid pages"
      else
-        putStrLn $ "Failure, " ++ show failed ++ " invalid pages"
+        putStrLn $ "\nFailure, " ++ show failed ++ " invalid pages"
 
 
 checkOne :: String -> IO Int
 checkOne x = do
+    putChar '.'
     src <- openURL $ "http://validator.w3.org/check?uri=" ++ x
     let text = words $ innerText $ take 1 $ drop 1 $ dropWhile (~/= "<title>") $ parseTags src
         failed = not $ ["[Valid]"] `isPrefixOf` text
     when failed $
-        putStrLn $ "Invalid: " ++ x
+        putStrLn $ "\nInvalid: " ++ x
     return $ if failed then 1 else 0
