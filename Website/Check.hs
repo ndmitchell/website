cde2132e2b2cc5499f0a552a2535170684de617d
Neil Mitchell <ndmitchell@gmail.com>
2008-05-05 14:24:22 +0000

    Add a routine to check the validator on each page

---
+++ Website/Check.hs
@@ -0,0 +1,27 @@
+
+module Website.Check(check) where
+
+import Control.Monad
+import Data.List
+import Text.HTML.Download
+import Text.HTML.TagSoup
+
+
+-- list all the pages that aren't valid
+check :: [String] -> IO ()
+check xs = do
+    failed <- liftM sum $ mapM checkOne xs
+    if failed == 0 then
+        putStrLn $ "Success, no invalid pages"
+     else
+        putStrLn $ "Failure, " ++ show failed ++ " invalid pages"
+
+
+checkOne :: String -> IO Int
+checkOne x = do
+    src <- openURL $ "http://validator.w3.org/check?uri=" ++ x
+    let text = words $ innerText $ take 1 $ drop 1 $ dropWhile (~/= "<title>") $ parseTags src
+        failed = not $ ["[Valid]"] `isPrefixOf` text
+    when failed $
+        putStrLn $ "Invalid: " ++ x
+    return $ if failed then 1 else 0
